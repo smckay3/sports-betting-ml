53b813acd6ab002af09ea3e670676b5332790827
Rob Brady <bradybridge@gmail.com>
2022-10-16 12:57:10 -0400

    Update main.py
    Simple ELO Update Added

--- main.py
+++ main.py
@@ -81,6 +81,16 @@ class Game:
                 player.player_id, (player.name, 0.5)
             )[1]
 
+        """
+        RB - We shouldn't average the player's ratings for the team average.
+        If the starting 5 plays most of the game and has a high rating, the
+        team's rating should be high. I think we should either:
+            a) Save playtime for players as they play and use this stat
+            to predict their average playtime in a game for rating purposes
+            OR
+            b) Pick some number (say 10) and normalize all teams to have this
+            number of players (choose the top N ratings on the team).
+        """
         self.home_team_rating = sum(
             map((lambda player: player.pre_rating), self.home_team)
         ) / len(self.home_team)
@@ -88,7 +98,28 @@ class Game:
         self.away_team_rating = sum(
             map((lambda player: player.pre_rating), self.away_team)
         ) / len(self.away_team)
+        
+        "Star Player Adjustment"
+        home_sp_rating = 0
+        away_sp_rating = 0
+        for player in self.home_team:
+            if player.pre_rating > home_sp_rating:
+                home_sp_rating = player.pre_rating
+        
+        for player in self.away_team:
+            if player.pre_rating > away_sp_rating:
+                away_sp_rating = player.pre_rating
+                
+        self.home_team_rating = (self.home_team_rating * len(self.home_team)
+                                 + home_sp_rating)/(len(self.home_team) + 1)
+        self.away_team_rating = (self.away_team_rating * len(self.away_team)
+                                 + away_sp_rating)/(len(self.away_team) + 1)
+        
+        "END Star Player Adjustment"
+        
+        "Crude Rating Update"
 
+        """
         total_rating = self.home_team_rating + self.away_team_rating
         expected_outcome_home = self.home_team_rating / total_rating
         expected_outcome_away = self.away_team_rating / total_rating
@@ -115,6 +146,42 @@ class Game:
                 player.pre_rating + (player.seconds_played / time_factor) * diff_away
             )
             player_ratings[player.player_id] = (player.name, player.post_rating)
+        
+        """
+            
+        "END Crude Rating Update"
+        
+        "ELO Update"
+        
+        "Define the K-factor, which is akin to the learning rate (i.e. the"
+        "maximum ELO gained or lost from one game)."
+        K = .02
+        
+        "Define the spread between two teams s.t. a team whose ELO rating is"
+        "larger by this amount is expected to win 75% of the time."
+        spread = .1
+        
+        "Calculate expected chance for home team to win"
+        E_home = 1 / (1 + 10 ** ((self.away_team_rating - self.home_team_rating)/spread))
+        
+        "Apply rating update based on time played"
+        win = 0
+        if self.home_team_score > self.away_team_score:
+            win = 1
+        elif self.home_team_score < self.away_team_score:
+            win = 0
+        else:
+            win = .5
+        
+        for player in self.home_team:
+            player.post_rating = player.pre_rating + K * (win - E_home) * player.seconds_played / (60 * 48)
+            player_ratings[player.player_id] = (player.name, player.post_rating)
+            
+        for player in self.away_team:
+            player.post_rating = player.pre_rating + K * (E_home - win) * player.seconds_played / (60 * 48)
+            player_ratings[player.player_id] = (player.name, player.post_rating)
+        
+        "END ELO Update"
 
     def __str__(self) -> str:
         return f"(home: {self.home_team}, away: {self.away_team})"
@@ -135,5 +202,13 @@ for game in tqdm(games.values()):
     if game.home_team and game.away_team:
         game.set_ratings(player_ratings)
 
-for name, rating in player_ratings.values():
-    print(name, rating)
+"Sort players by rating" "RB - I have no idea what this does but it's not doing what I want. Found it online somewhere."
+sorted_ratings = {v for k, v in sorted(player_ratings.items(), key = lambda item: item[1])}
+
+print(sorted_ratings)
+
+#for name, rating in sorted_ratings.values():
+    #print(name, rating)
+
+#for name, rating in player_ratings.values():
+    #print(name, rating)
